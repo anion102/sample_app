c24af708a097fde796068129e4f5c61275af4d0a
kanlijun <kanlijun@mobanker.com>
2017-03-29 20:52:35 +0800

    user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
   def new
+    @user =User.new
+    # debugger
   end
+
+  def show
+    @user = User.find(params[:id])
+
+    # debugger
+  end
+
+  def create
+    @user= User.new(user_param)
+    if @user.save
+      #处理注册成功的情况
+      flash[:success] = "Welcome to Sample App!"
+      # redirect_to user_url(@user)
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_param
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test 'invalid signuo infromation' do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params:{
+          user:{
+              name:'',
+              email:'user@invalid',
+              password:'foo',
+              password_confirmation:'bar'
+          }
+      }
+      assert_template 'users/new'
+
+      assert_select "form[action=?]",'/signup'
+    end
+  end
+end
